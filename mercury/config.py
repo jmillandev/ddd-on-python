7c068bf682916a0cb6430099e06457b0aa9450d8
Jesús Millán <jgmc3012@gmail.com>
2023-06-22 01:42:42 -0400

    Firt commit

---
+++ mercury/config.py
@@ -0,0 +1,26 @@
+from typing import List, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, validator
+
+
+class Settings(BaseSettings):
+    API_PREFIX: str = "/api"
+     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[str, List[str]]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str = "Mercury"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
