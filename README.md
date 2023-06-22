2c1c7057db53f020c3874dec457b7f1f97e13e99
Jesús Millán <jgmc3012@gmail.com>
2023-06-22 19:43:48 -0400

    :star: Config Migrations

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mercury-api
+# Database
+
+## Technologies
+- [PostgreSQL](https://www.postgresql.org/)
+- [Alembic](https://alembic.sqlalchemy.org/en/latest/)
+- [SQLAlchemy](https://www.sqlalchemy.org/)
+
+## How to run a new migration
+1. Create a new migration file
+```bash
+$ alembic revision --autogenerate -m "migration name"
+```
+2. Run the migration
+```bash
+$ alembic upgrade head
+```
--- mercury/config.py
+++ mercury/config.py
@@ -1,10 +1,11 @@
-from typing import List, Union
+from typing import List, Union, Optional, Dict, Any
 
-from pydantic import AnyHttpUrl, BaseSettings, validator
+from pydantic import AnyHttpUrl, BaseSettings, validator, PostgresDsn
 
 
 class Settings(BaseSettings):
     API_PREFIX: str = "/api"
+    PROJECT_NAME: str = "Mercury"
      # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
@@ -17,7 +18,23 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    PROJECT_NAME: str = "Mercury"
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
 
     class Config:
         case_sensitive = True
