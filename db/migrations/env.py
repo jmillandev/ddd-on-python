0c7ad46d15161358741479a5fe3efcb3b59c702f
Jesús Millán <jgmc3012@gmail.com>
2023-09-20 23:50:40 -0400

    [TAS-21] :bug: Interface Error DB - https://github.com/MagicStack/asyncpg/issues/863#issuecomment-1227086997

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -2,7 +2,7 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from db.models import Base
--- db/session.py
+++ db/session.py
@@ -1,8 +1,9 @@
 # from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
 from mercury.config import settings
+from sqlalchemy import pool
 
-engine = create_async_engine(settings.DATABASE_URI, echo=True)
+engine = create_async_engine(settings.DATABASE_URI, echo=True, poolclass=pool.NullPool)
 LocalSession = async_sessionmaker(
     engine, autoflush=False, expire_on_commit=False)
 
