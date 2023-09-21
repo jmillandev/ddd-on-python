8fa09541e1465d3561397c8e3b7e2176367c7a1b
Jesús Millán <jgmc3012@gmail.com>
2023-09-21 03:34:23 -0400

    [TAS-22] :star: Add Failure Test cases

--- tests/users/test_controllers/test_sing_in.py
+++ tests/users/test_controllers/test_sing_in.py
@@ -36,3 +36,31 @@ class TestSignIn:
         assert json_user['name'] == user.name
         assert json_user['full_name'] == user.full_name
         assert json_user['pronoun'] == user.pronoun
+
+    async def test_email_doesnt_exists(self, client: AsyncClient, db_session: AsyncSession) -> None:
+        params = {'username': fake.email(), 'password': fake.password(), 'grant_type': 'password'}
+
+        response = await client.post(f"{settings.API_PREFIX}/v1/sign-in", json=params)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, response.text
+
+        json_response = response.json()
+        assert len(json_response['detail']) == 1
+        error_response = json_response['detail'][0]
+        assert error_response['msg'] == 'Incorrect username or password'
+        assert error_response['source'] == 'unknown'
+
+    async def test_invalid_password(self, client: AsyncClient, db_session: AsyncSession) -> None:
+        password = fake.password()
+        user = await UserFactory(password=password)
+        params = {'username': user.email, 'password': 'Superpass', 'grant_type': 'password'}
+
+        response = await client.post(f"{settings.API_PREFIX}/v1/sign-in", json=params)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, response.text
+
+        json_response = response.json()
+        assert len(json_response['detail']) == 1
+        error_response = json_response['detail'][0]
+        assert error_response['msg'] == 'Incorrect username or password'
+        assert error_response['source'] == 'unknown'
