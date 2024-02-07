95ab018904b809ea06606fbb3d4fa9f51128350e
Jesús Millán <jgmc3012@gmail.com>
2024-02-07 14:03:29 -0400

    [TAS-12] :star: Add Retrieve Controller Test case

---
+++ tests/apps/planner/accounts/test_controllers/test_find.py
@@ -0,0 +1,64 @@
+import pytest
+from faker import Faker
+from fastapi import status
+from httpx import AsyncClient
+from kink import di
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from apps.planner.backend.config import settings
+from src.planner.users.domain.repository import UserRepository
+from tests.apps.planner.shared.auth import AuthAsUser
+from tests.src.planner.shared.factories.accounts import AccountFactory
+from tests.src.planner.users.factories import UserFactory
+
+fake = Faker()
+
+pytestmark = pytest.mark.anyio
+
+
+class TestCreateAccountController:
+    def setup_method(self):
+        self._user = UserFactory.build()
+        self._account = AccountFactory.build()
+        self._url = f"{settings.API_PREFIX}/v1/accounts/{self._account.id.primitive}"
+
+    async def test_success(
+        self, client: AsyncClient, sqlalchemy_session: AsyncSession
+    ) -> None:
+        await di[UserRepository].create(self._user)  # type: ignore[type-abstract]
+
+        response = await client.get(self._url, auth=AuthAsUser(self._user.id))
+
+        assert response.status_code == status.HTTP_200_OK, response.text
+        assert response.json() == {
+            "id": self._account.id.primitive,
+            "name": self._account.name.primitive,
+            "currency": self._account.currency.primitive,
+            "balance": self._account.balance.primitive,
+        }
+
+    async def test_should_return_not_found(
+        self, client: AsyncClient, sqlalchemy_session: AsyncSession
+    ) -> None:
+        response = await client.get(self._url, auth=AuthAsUser(self._user.id))
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
+        assert response.json() == {
+            "id": self._account.id.primitive,
+            "name": self._account.name.primitive,
+            "currency": self._account.currency.primitive,
+            "balance": self._account.balance.primitive,
+        }
+
+    async def test_should_return_unauthorized_missing_token(
+        self, client: AsyncClient, sqlalchemy_session: AsyncSession
+    ) -> None:
+        response = await client.get(self._url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.text
+
+        json_response = response.json()
+        assert len(json_response["detail"]) == 1
+        error_response = json_response["detail"][0]
+        assert error_response["msg"] == "Is required"
+        assert error_response["source"] == "access_token"
